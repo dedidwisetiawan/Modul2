d344311a77d7f79f22ff447d2a29e7d1cd106950
dedidwisetiawan <32447378+dedidwisetiawan@users.noreply.github.com>
2017-10-25 23:12:37 +0800

    Masukan Luas rumah

--- Tugas 0204.cpp
+++ Tugas 0204.cpp
@@ -4,4 +4,5 @@ using namespace std;
 int main(){
 double
 luas(42),jumlah_box(22.3885),luas_box(1.87596),biaya_parket(11194.3),biaya_pasang,total_biaya(12034.3);
+	cout<<"Masukan Luas Rumah(m2)\t:";
 }
