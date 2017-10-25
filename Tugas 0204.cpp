e67c18ea141fa26fbfbbf9f0e464576acc3eb191
dedidwisetiawan <32447378+dedidwisetiawan@users.noreply.github.com>
2017-10-25 23:14:18 +0800

    Menghitung Luas

--- Tugas 0204.cpp
+++ Tugas 0204.cpp
@@ -5,4 +5,8 @@ int main(){
 double
 luas(42),jumlah_box(22.3885),luas_box(1.87596),biaya_parket(11194.3),biaya_pasang,total_biaya(12034.3);
 	cout<<"Masukan Luas Rumah(m2)\t:";
+	cin>>luas;
+	cout<<"Luas Rumah\t\t:"<<luas<<"m2/n";
+	luas_box=1.87596;
+	jumlah_box=luas/luas_box;
 }
